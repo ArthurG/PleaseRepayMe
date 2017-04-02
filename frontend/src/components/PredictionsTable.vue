f20c8e919faacd78001fd772ce23e3b53cd6d5a5
Arthur Guo <arthur.guo@uwaterloo.ca>
2017-04-01 20:58:37 -0400

    Add frontend table and header

---
+++ frontend/src/components/PredictionsTable.vue
@@ -0,0 +1,144 @@
+<template>
+
+<div id="PredictionsTable">
+  <div class="justify-content-centermy-1 row">
+
+    <b-form-fieldset horizontal label="Rows per page" class="col-6" :label-size="6">
+      <b-form-select :options="[{text:5,value:5},{text:10,value:10},{text:15,value:15}]" v-model="perPage">
+      </b-form-select>
+    </b-form-fieldset>
+
+    <b-form-fieldset horizontal label="Filter" class="col-6" :label-size="2">
+      <b-form-input v-model="filter" placeholder="Type to Search"></b-form-input>
+    </b-form-fieldset>
+  </div>
+
+  <!-- Main table element -->
+  <b-table striped hover :items="items" :fields="fields" :current-page="currentPage" :per-page="perPage" :filter="filter">
+     <template slot="borrower_name" scope="item">
+        <a v-bind:href="item.item.original_thread_url">{{item.item.borrower_name}}</a>
+     </template>
+     <template slot="lender_name" scope="item">
+        <a v-bind:href="item.item.lender_comment_url">{{item.item.lender_name}}</a>
+     </template>
+     <template slot="status" scope="item">
+        <a v-bind:href="item.item.settle_thread_url">{{item.item.status}}</a>
+     </template>
+  </b-table>
+
+  <div class="justify-content-center row my-1">
+    <b-pagination size="md" :total-rows="this.items.length" :per-page="perPage" v-model="currentPage" />
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'PredictionsTable',
+  data () {
+    return {
+      'items': [
+        {
+          'agree_comment_url': 'true',
+          'borrower_name': 'arthur',
+          'date_requested': 'Sat, 01 Apr 2017 12:18:37 GMT',
+          'lender_comment_url': 'google.ca',
+          'lender_name': 'andrew',
+          'original_thread_url': 'youtube.com',
+          'repayment_probability': 1.235,
+          'settle_thread_url': 'gsgdfhsdf.ca',
+          'status': 1,
+          'transaction_id': 1
+        },
+        {
+          'agree_comment_url': 'true',
+          'borrower_name': 'arthur',
+          'date_requested': 'Sat, 01 Apr 2017 12:18:37 GMT',
+          'lender_comment_url': 'google.ca',
+          'lender_name': 'andrew',
+          'original_thread_url': 'youtube.com',
+          'repayment_probability': null,
+          'settle_thread_url': 'gsgdfhsdf.ca',
+          'status': 1,
+          'transaction_id': 2
+        },
+        {
+          'agree_comment_url': 'true',
+          'borrower_name': 'arthur',
+          'date_requested': 'Sat, 01 Apr 2017 12:18:37 GMT',
+          'lender_comment_url': 'google.ca',
+          'lender_name': 'andrew',
+          'original_thread_url': 'youtube.com',
+          'repayment_probability': null,
+          'settle_thread_url': 'gsgdfhsdf.ca',
+          'status': 1,
+          'transaction_id': 3
+        },
+        {
+          'agree_comment_url': 'true',
+          'borrower_name': 'arthur',
+          'date_requested': 'Sat, 01 Apr 2017 12:18:37 GMT',
+          'lender_comment_url': 'google.ca',
+          'lender_name': 'andrew',
+          'original_thread_url': 'youtube.com',
+          'repayment_probability': null,
+          'settle_thread_url': 'gsgdfhsdf.ca',
+          'status': 1,
+          'transaction_id': 4
+        },
+        {
+          'agree_comment_url': 'true',
+          'borrower_name': 'arthur',
+          'date_requested': 'Sat, 01 Apr 2017 12:18:37 GMT',
+          'lender_comment_url': 'google.ca',
+          'lender_name': 'andrew',
+          'original_thread_url': 'youtube.com',
+          'repayment_probability': null,
+          'settle_thread_url': 'gsgdfhsdf.ca',
+          'status': 1,
+          'transaction_id': 5
+        }
+      ],
+      fields: {
+        transaction_id: {
+          label: 'id',
+          sortable: true
+        },
+        borrower_name: {
+          label: 'Borrower Name',
+          sortable: true
+        },
+        lender_name: {
+          label: 'Lender Name'
+        },
+        date_requested: {
+          label: 'Date Requested',
+          sortable: true
+        },
+        repayment_probability: {
+          label: 'Repayment Probability',
+          sortable: true
+        },
+        status: {
+          label: 'Status',
+          sortable: true
+        }
+      },
+      currentPage: 1,
+      perPage: 10,
+      filter: null
+    }
+  },
+  computed: {
+    compItems: function () {
+      this.items
+    }
+  },
+  methods: {
+    details (item) {
+      alert(JSON.stringify(item))
+    }
+  }
+}
+</script>
---
+++ frontend/src/components/TopNav.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+  <b-navbar toggleable type="inverse" variant="success">
+
+  <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+
+  <b-link class="navbar-brand" to="#">
+  <span>BootstrapVue</span>
+  </b-link>
+
+  <b-collapse is-nav id="nav_collapse">
+
+  <b-nav is-nav-bar>
+  <b-nav-item>Support</b-nav-item>
+  <b-nav-item>Docs</b-nav-item>
+  <b-nav-item>Contact Us</b-nav-item>
+  </b-nav>
+
+  <b-nav is-nav-bar class="ml-auto">
+
+  <!-- Navbar dropdowns -->
+  <b-nav-item-dropdown text="Lang" right-alignment>
+    <b-dropdown-item to="#">EN</b-dropdown-item>
+    <b-dropdown-item to="#">ES</b-dropdown-item>
+    <b-dropdown-item to="#">RU</b-dropdown-item>
+    <b-dropdown-item to="#">FA</b-dropdown-item>
+  </b-nav-item-dropdown>
+
+  <b-nav-item-dropdown right-alignment>
+
+  <!-- Using text slot -->
+    <template slot="text">
+      <span style="font-weight: bold;">User</span>
+    </template>
+
+    <b-dropdown-item to="#">Profile</b-dropdown-item>
+    <b-dropdown-item to="#">Signout</b-dropdown-item>
+  </b-nav-item-dropdown>
+
+  </b-nav>
+  </b-collapse>
+  </b-navbar>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'TopNav'
+}
+</script>
+
