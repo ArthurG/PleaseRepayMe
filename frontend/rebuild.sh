62dfca8ed86ef9634a3e0283e3d4b2e5100aa495
Arthur Guo <arthur.guo@uwaterloo.ca>
2017-08-20 17:59:24 -0400

    Make rebuild script

---
+++ frontend/rebuild.sh
@@ -0,0 +1,6 @@
+npm run build
+cp dist/static/css/* ../backend/app/static/css/
+cp dist/static/js/* ../backend/app/static/js/
+cp dist/static/img/* ../backend/app/static/img/
+cp dist/index.html ../backend/app/templates/
+
