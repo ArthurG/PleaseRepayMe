cf584088c415b0dbbb288c22250d05361fb9d518
guoarthur <arthur.guo@uwaterloo.ca>
2017-08-22 21:51:39 -0400

    Edit about page

--- frontend/src/components/Modals.vue
+++ frontend/src/components/Modals.vue
@@ -9,38 +9,41 @@
        <b-modal id="modal-about" title="About" hide-footer>
 
         <p1 class="summary">
- TL;DR: We use <a href="https://en.wikipedia.org/wiki/Machine_learning">Machine Learning</a> to predict loan outcomes on Reddit. Our algorithm seems to be quite good at successfully predicting loan outcomes. We would love for you to try it out!
+ TL;DR: We use <a href="https://en.wikipedia.org/wiki/Machine_learning">Machine Learning</a> to predict loan outcomes on Reddit's /r/borrow. Our algorithm is quite good at predicting loan outcomes. We would love for you to try it out!
         </p1>
 
         <hr/>
         <h3 class="info">
 So you think you know what makes a good borrower? 
         </h3>
+        <h6 class="info">
+Well, at least 300 Redditors don't.
+        </h6>
 
         <p class="info">
-Having facilitated more than 9000 loans, <a href="http://reddit.com/r/borrow/">www.reddit.com/r/borrow/</a> has a ton of information on what makes a Redditor trustworthy (read: not a scammer, likely to repay their loans). PleaseRepay.me seeks to capitalize on this information, and use it to predict the outcome of a loan. 
+Having facilitated more than 9000 loans, <a href="http://reddit.com/r/borrow/">www.reddit.com/r/borrow/</a> holds a ton of information on what makes a Redditor trustworthy (read: not a scammer on /r/borrow and is likely to repay their loans). PleaseRepay.me seeks to capitalize on this information, and use it to predict the outcome of a loan. 
         </p>
         <p class="info"> 
-Using all posts on /r/borrow, we categorize "[REQ]" posts into three buckets:
+We categorize all "[REQ]" on /r/borrow into three buckets:
           <ul>
               <li> Repaid </li>
               <li> Unpaid </li>
-              <li> Loan Unfulfilled (No lender willing to take the loan) </li>
+              <li> Loan Unfulfilled (No lender willing to give a loan) </li>
           </ul>
         </p>
         <p class="info">
-Using a process known as <a href="https://en.wikipedia.org/wiki/Machine_learning">Machine Learning</a>, we analyzed [REQ]esters for more than 30 characteristics. We asked the question: If a user has a certain characteristic, does it correlate to the user being in a certain bucket? </p>
+Using a process known as <a href="https://en.wikipedia.org/wiki/Machine_learning">Machine Learning</a>, we analyzed [REQ]uesters for more than 30 characteristics. We asked the question: If a user has a certain characteristic, does it correlate to the user being in a certain bucket? </p>
         <p class="info">
-After taking note of the characteristics the characteristics that correlate with each bucket, we can predict what bucket any Reddit user belongs to. 
+After taking note of the characteristics that correlate with each bucket, we can predict what bucket any old Redditor belongs to. 
         </p>
         <p class="info"> 
-Our algorithm is quite good at predicting outcomes, with an <a href="https://en.wikipedia.org/wiki/F1_score">F1 Score</a> of more than 90%.  
+Our algorithm is quite good at predicting outcomes, with an <a href="https://en.wikipedia.org/wiki/F1_score">F1 Score</a> of more than 95%.  
         </p>
         <p class="info"> 
 Special shoutout goes to <a href="https://www.reddit.com/user/Stuck_In_the_Matrix"> /u/Stuck_In_the_Matrix </a> for providing the data for this project!
         </p>
         <h5>
-Try it out! It's pretty cool if you ask me :)
+Try it out! It's pretty cool :)
         </h5>
       </b-modal>
     </div>
